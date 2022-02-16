2184879afcccaebfdfb9942fc6844df7dfc2fbbd
Anjani <pranav.ch.12@outlook.com>
2022-02-16 13:41:47 +0100

    Fixed Collapse Bug

--- src/js/collapse.js
+++ src/js/collapse.js
@@ -1,8 +1,9 @@
 window.addEventListener('click', e => {
     let collapseHeading;
-
-    if (e.target.classList.contains('collapse-heading')) collapseHeading = e.target;
-    else if (e.target.parentElement.classList.contains('collapse-heading')) collapseHeading = e.target.parentElement;
+    if (e.target.tagName !== 'HTML') {
+        if (e.target.classList.contains('collapse-heading')) collapseHeading = e.target;
+        else if (e.target.parentElement.classList.contains('collapse-heading')) collapseHeading = e.target.parentElement;
 
     if(collapseHeading) collapseHeading.parentElement.classList.toggle('toggled');
+    }
 });
