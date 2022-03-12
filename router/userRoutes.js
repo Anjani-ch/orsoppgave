34cd9a5b56ee3110ae3c78582b3f386b80e98e16
Anjani <pranav.ch.12@outlook.com>
2022-03-12 14:24:42 +0100

    updated user routes

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { handleLogin, handleSignup, handleLogout } = require('../controllers/authController.js');
-const { deleteUser, updateUser } = require('../controllers/userController.js');
+const { deleteUser } = require('../controllers/userController.js');
 
 const router = express.Router();
 
@@ -16,8 +16,5 @@ router.post('/logout', (req, res) => handleLogout(req, res));
 
 // User Delete Route
 router.post('/delete', (req, res) => deleteUser(req, res));
-
-// User Update Route
-router.post('/delete', (req, res) => updateUser(req, res));
 
 module.exports = router;
