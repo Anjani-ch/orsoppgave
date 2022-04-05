dd65e7f1e14b5b3c553c2d916cd6c88bf152f793
Anjani <pranav.ch.12@outlook.com>
2022-04-05 17:24:56 +0200

    Fixed dashboard table issue

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -3,6 +3,12 @@ import createAlert from './utilities/createAlert.js';
 const dashboard = document.querySelector('#dashboard');
 
 if(dashboard) {
+    const addNotificationBtn = document.querySelector('#add-notification-btn');
+    const addEmailBtn = document.querySelector('#add-email-btn');
+    const addNotificationForm = document.querySelector('#add-notification-form');
+    const addEmailForm = document.querySelector('#add-email-form');
+
+
     dashboard.addEventListener('click', e => {
         if(e.target.classList.contains('admin-action')) {
             const parentEl = e.target.parentElement;
@@ -29,33 +35,49 @@ if(dashboard) {
             fetch(request.endpoint, { method: request.method })
                 .then(res => res.json())
                 .then(data => {
+                    const userTable = document.querySelector('#user-table');
+                    const adminTable = document.querySelector('#admin-table');
+
                     const currTableRow = parentEl.parentElement;
                     const parentTable = currTableRow.parentElement;
                     
                     let tableRowsInTable;
 
                     if(request.method === 'PUT') {
-                        const adminTable = document.querySelector('#admin-table');
                         const tableRow = document.createElement('DIV');
 
-                        const { username, email, isAdmin, id } = data.user;
+                        const { username, email, isAdmin, _id } = data.user;
+
+                        let tableRowsInAdminTable;
 
                         tableRow.className = 'table-row text-center';
 
                         tableRow.innerHTML = `
                             <p>${username}</p>
                             <p>${email}</p>
-                            <p data-userId="${id}" data-isAdmin="${isAdmin}"><i class="fa-solid fa-trash delete delete-user"></i></p>
+                            <p data-userId="${_id}" data-isAdmin="${isAdmin}"><i class="fa-solid fa-trash delete delete-user admin-action"></i></p>
                         `;
                         
                         adminTable.appendChild(tableRow);
+
+                        tableRowsInAdminTable = Array.from(adminTable.children).filter(child => child.classList.contains('table-row'));
+
+                        if(tableRowsInAdminTable.length === 2) {
+                            adminTable.classList.remove('d-none-important');
+                        }
                     }
 
                     currTableRow.remove();
 
                     tableRowsInTable = Array.from(parentTable.children).filter(child => child.classList.contains('table-row'));
 
-                    if(tableRowsInTable.length === 1) parentTable.remove();
+                    if(tableRowsInTable.length === 1) {
+                        if(parentTable === userTable) {
+                            parentTable.remove();
+                        } else if(parentTable === adminTable) {
+                            parentTable.classList.add('d-none-important');
+                        }
+                    }
 
                     if(request.method === 'PUT') alert.message = 'Promoted user';
                     if(request.method === 'DELETE') alert.message = 'Deleted user';
@@ -72,5 +94,39 @@ if(dashboard) {
                 })
                 .finally(_ => dashboard.prepend(createAlert(alert.message, alert.type)));
         }
+    });
+
+    addNotificationBtn.addEventListener('click', e => {
+        addNotificationForm.classList.toggle('d-none');
+
+        if(!addNotificationForm.classList.contains('d-none')) {
+            e.target.innerText = 'Close';
+        } else {
+            e.target.innerText = 'Send Message';
+        }
+    });
+
+    addEmailBtn.addEventListener('click', e => {
+        addEmailForm.classList.toggle('d-none');
+
+        if(!addEmailForm.classList.contains('d-none')) {
+            e.target.innerText = 'Close';
+        } else {
+            e.target.innerText = 'Send Message';
+        }
+    });
+
+    addNotificationForm.addEventListener('submit', e => {
+        const { notificationMsg, notificationDate } = e.target;
+
+        console.log(notificationMsg)
+        console.log(notificationDate)
+    });
+
+    addEmailForm.addEventListener('submit', e => {
+        const { emailSubject, emailDate } = e.target;
+
+        console.log(emailSubject)
+        console.log(emailDate)
     });
 }
